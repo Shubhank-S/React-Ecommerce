1e6f4586f020f0837ca64d4de1a01f3c01a2487a
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-01-24 00:10:52 +0530

    Routing setup completed via RRDv6

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <main>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>This is Home Page</h1>} />
+          <Route path="/men" element={<h1>This is men Page</h1>} />
+          <Route path="/woman" element={<h1>This is woman Page</h1>} />
+          <Route path="/kids" element={<h1>This is kids Page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import cart from "../../assets/cart_icon.png";
 import "./Navbar.css";
 
 function Navbar() {
-  const [menu, setMenu] = useState("shop");
+  const [menu, setMenu] = useState("home");
   return (
     <header className="navbar">
       <section className="navbar_logo">
@@ -12,8 +12,8 @@ function Navbar() {
         <p>Ecommerce</p>
       </section>
       <ul className="navbar_links">
-        <li onClick={() => setMenu("shop")}>
-          Shop {menu === "shop" ? <hr /> : ""}
+        <li onClick={() => setMenu("home")}>
+          Home {menu === "home" ? <hr /> : ""}
         </li>
         <li onClick={() => setMenu("mens")}>
           Men {menu === "mens" ? <hr /> : ""}
