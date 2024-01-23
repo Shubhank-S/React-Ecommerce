6434190c02e254b7d873bf1cf8dc671ab560487a
Shubhank Sharma <shubhanksharma345@gmail.com>
2024-01-23 22:36:34 +0530

    Navbar component completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import cart from "../../assets/cart_icon.png";
 import "./Navbar.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [menu, setMenu] = useState("shop");
+  return (
+    <header className="navbar">
+      <section className="navbar_logo">
+        <img src={logo} alt="No Logo" />
+        <p>Ecommerce</p>
+      </section>
+      <ul className="navbar_links">
+        <li onClick={() => setMenu("shop")}>
+          Shop {menu === "shop" ? <hr /> : ""}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          Men {menu === "mens" ? <hr /> : ""}
+        </li>
+        <li onClick={() => setMenu("womans")}>
+          Woman {menu === "womans" ? <hr /> : ""}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          Kids {menu === "kids" ? <hr /> : ""}
+        </li>
+      </ul>
+      <section className="navbar_cart">
+        <button>Login</button>
+        <img src={cart} alt="No Cart" />
+        <div className="navbar_cart_count">0</div>
+      </section>
+    </header>
+  );
 }
 
 export default Navbar;
